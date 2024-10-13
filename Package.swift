945a262b33365c08a8b4c0e0b1b4db0fddee048f
Dmitriy Mikitenko <>
2024-10-14 01:37:32 +0300

    Update BitcoinCore & BitcoinCashKit

--- Package.swift
+++ Package.swift
@@ -14,8 +14,8 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/horizontalsystems/BitcoinCore.Swift.git", .upToNextMajor(from: "3.0.0")),
-        .package(url: "https://github.com/horizontalsystems/BitcoinCashKit.Swift.git", .upToNextMajor(from: "3.0.0")),
+        .package(url: "https://github.com/DmitriyMikitenko/BitcoinCore.Swift.git", branch: "dextrade"),
+        .package(url: "https://github.com/DmitriyMikitenko/BitcoinCashKit.Swift.git", branch: "dextrade"),
         .package(url: "https://github.com/apple/swift-protobuf.git", from: "1.6.0"),
     ],
     targets: [
